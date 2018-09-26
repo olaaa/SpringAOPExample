ad1227cd3194c0f5c1d75bceb466cb2dadd22972
Karavsheva Olga <karavshevaoe@altarix.ru>
2018-09-26 15:24:29 +0400

    Initial commit

---
+++ src/main/java/com/journaldev/spring/model/Employee.java
@@ -0,0 +1,25 @@
+package com.journaldev.spring.model;
+
+import com.journaldev.spring.aspect.Loggable;
+
+public class Employee {
+
+	private String name;
+	
+	public String getName() {
+		if (true) {
+			throw new RuntimeException("Name Exception");
+		}
+		return name;
+	}
+
+	@Loggable
+	public void setName(String nm) {
+		this.name=nm;
+	}
+	
+	public void throwException(){
+		throw new RuntimeException("Dummy Exception");
+	}
+	
+}
